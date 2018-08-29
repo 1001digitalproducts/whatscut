da53ef198bc685efb6896bb6b534a801b5b78424
rroossyyiidd <rroossyyiidd@gmail.com>
2018-08-29 10:10:54 +0700

    styling

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Dimensions} from 'react-native';
+import {StyleSheet, View, Dimensions, TouchableOpacity} from 'react-native';
 import {
-    Card, CardItem, Body, Text, Textarea, Button, Form, Label
+    Card, CardItem, Body, Text, Textarea, Form, Label, Icon
 } from 'native-base';
 import PhoneInput from 'react-native-phone-input'
 import ModalPickerImage from './ModalPickerImage';
@@ -46,80 +46,133 @@ export default class App extends Component {
         })
     };
 
+    sendMessage = () => {
+        console.log('send message!');
+    };
+
+    openMenu = () => {
+        console.log('open menu!');
+    };
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Whats Cut!</Text>
-                <Card style={styles.cardStyle}>
-                    <CardItem style={styles.cardStyle}>
-                        <Body>
-                        <View style={styles.formNumber}>
-                            <PhoneInput
-                                initialCountry={`id`}
-                                ref={(ref) => {
-                                    this.phone = ref;
-                                }}
-                                onPressFlag={this.onPressFlag}
-                                onChangePhoneNumber={this.changePhoneNumber}
-                                value={this.state.notelp}
-                            />
-                        </View>
-                        <Form
-                            style={styles.formStyle}>
-                            <Label style={{color: '#fff'}}>Alamat</Label>
-                            <Textarea
-                                rowSpan={5}
-                                value={this.state.message}
-                                onChangeText={this.changeMessage}
-                                placeholder="Isi pesan"
-                            />
-                        </Form>
-                        </Body>
-                    </CardItem>
-                </Card>
+            <View style={{flex: 1}}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => this.openMenu}>
+                        <Icon style={{color: '#338a3e'}} name='dots-vertical' type={`MaterialCommunityIcons`}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>Whats Cut!</Text>
+                    <Card style={styles.cardStyle}>
+                        <CardItem style={styles.cardStyle}>
+                            <Body>
+                            <View style={styles.formNumber}>
+                                <PhoneInput
+                                    initialCountry={`id`}
+                                    ref={(ref) => {
+                                        this.phone = ref;
+                                    }}
+                                    onPressFlag={this.onPressFlag}
+                                    onChangePhoneNumber={this.changePhoneNumber}
+                                    value={this.state.notelp}
+                                />
+                            </View>
+                            <Form
+                                style={styles.formStyle}>
+                                <Label style={{color: '#fff'}}>Alamat</Label>
+                                <Textarea
+                                    rowSpan={5}
+                                    value={this.state.message}
+                                    onChangeText={this.changeMessage}
+                                    placeholder="Isi pesan"
+                                />
+                            </Form>
+                            <TouchableOpacity
+                                onPress={() => this.sendMessage}
+                                style={styles.viewButtonStyle}>
+                                <View style={styles.buttonStyle}>
+                                    <Icon style={{color: '#e8f5e9'}} name='send' type={`MaterialIcons`}/>
+                                </View>
+                            </TouchableOpacity>
+                            </Body>
+                        </CardItem>
+                    </Card>
 
-                <ModalPickerImage
-                    ref={(ref) => {
-                        this.myCountryPicker = ref;
-                    }}
-                    data={this.state.pickerData}
-                    onChange={(country) => {
-                        this.selectCountry(country)
-                    }}
-                    cancelText='Cancel'
-                />
-
+                    <ModalPickerImage
+                        ref={(ref) => {
+                            this.myCountryPicker = ref;
+                        }}
+                        data={this.state.pickerData}
+                        onChange={(country) => {
+                            this.selectCountry(country)
+                        }}
+                        cancelText='Cancel'
+                    />
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    header: {
+        display: 'flex',
+        backgroundColor: '#E1E2E1',
+        flexDirection: 'row-reverse',
+        padding: 10,
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff',
+        backgroundColor: '#E1E2E1',
     },
     welcome: {
         fontSize: 20,
         alignSelf: 'center',
         margin: 10,
-        color: '#D93D3C'
+        color: '#338a3e'
     },
     cardStyle: {
         borderRadius: 8,
         width: (width) - 20,
-        backgroundColor: '#D93D3C',
-        borderColor: '#D93D3C',
+        backgroundColor: '#338a3e',
+        borderColor: '#338a3e',
     },
     formStyle: {
         alignSelf: 'center',
         width: '90%',
-        marginBottom: 5
+        marginBottom: 5,
+        backgroundColor: '#66bb6a',
+        borderRadius: 4,
+        margin: 6,
+        padding: 5
     },
     formNumber: {
         alignSelf: 'center',
         width: '90%',
+        marginBottom: 5,
+        marginTop: 10,
+        backgroundColor: '#66bb6a',
+        borderRadius: 4,
+        height: 30,
+        padding: 5,
+    },
+    buttonStyle: {
+        backgroundColor: '#66bb6a',
+        borderRadius: 50,
+        height: 50,
+        width: 50,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    viewButtonStyle: {
+        alignSelf: 'center',
+        display: 'flex',
+        flexDirection: 'row-reverse',
+        width: '90%',
+        padding: 1
     }
 });
